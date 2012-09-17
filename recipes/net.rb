105e87b91556e00954898edd093966d4e8b4452d
mattray <matthewhray@gmail.com>
2012-09-17 11:50:43 -0500

    udev data bag support

--- recipes/net.rb
+++ recipes/net.rb
@@ -17,6 +17,18 @@
 # limitations under the License.
 #
 
+#search for any apt-cacher-ng caching proxies
+if Chef::Config[:solo]
+  Chef::Log.warn("This recipe attempts to use search with data bags. Chef Solo does not support this.")
+else
+  begin
+    udev_net = data_bag_item('udev', node.hostname)
+    node['udev']['net'] = udev_net['net'] if udev_net
+  rescue
+    Chef::Log.info "no 'udev' data bag entry for '#{node.hostname}' found."
+  end
+end
+
 template "/etc/udev/rules.d/70-persistent-net.rules" do
   source "70-persistent-net.rules.erb"
   mode "0644"
